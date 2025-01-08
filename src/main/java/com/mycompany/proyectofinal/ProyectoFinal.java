2f5bd8911e3dcd3f5bcc016df70a70007f0516c2
pacua <pacua@DESKTOP-J3ASS4F>
2025-01-08 22:27:12 +0100

    Primer Commit Código

--- src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
+++ src/main/java/com/mycompany/proyectofinal/ProyectoFinal.java
@@ -11,6 +11,6 @@ package com.mycompany.proyectofinal;
 public class ProyectoFinal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Parte de Codigo");
     }
 }
