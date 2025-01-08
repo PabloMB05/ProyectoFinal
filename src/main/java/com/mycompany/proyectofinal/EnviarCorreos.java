bbb0fbc114285989ae745568369ef8dd6ba7f9b2
pacua <pacua@DESKTOP-J3ASS4F>
2025-01-08 23:13:49 +0100

    Primer Commit

---
+++ src/main/java/com/mycompany/proyectofinal/EnviarCorreos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectofinal;
+
+/**
+ *
+ * @author pacua
+ */
+public class EnviarCorreos extends javax.swing.JFrame{
+    private  static String emailfrom = "";
+    private static String passwordfrom="";
+    private String emailto;
+    private String subject;
+    private String content;
+    
+    public EnviarCorreos(){
+        initComponents();
+    }
+}
